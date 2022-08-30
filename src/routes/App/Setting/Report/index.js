b848ebdad78397e06a88b9f45b18b679c08d67ae
Your Name <you@example.com>
2022-08-30 12:57:26 +0500

    uploadinf setting

---
+++ src/routes/App/Setting/Report/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Divider, Box } from '@material-ui/core';
+import PageContainer from '@jumbo/components/PageComponents/layouts/PageContainer';
+import { Link } from 'react-router-dom';
+const Report = () => {
+
+
+    return (
+        
+            <Box >
+<h1>
+    Report 
+</h1>
+            </Box>
+      
+    );
+};
+
+export default Report;
